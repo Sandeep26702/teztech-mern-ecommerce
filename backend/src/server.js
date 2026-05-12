4ab00604408b3d8427e173062b2249268394f03f
Sandeep26702 <ssharma931@rku.ac.in>
2026-05-12 14:49:56 +0530

    Fix payment stuts and delevry stuts bugs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
-import 'dotenv/config'; 
+import 'dotenv/config';
 import express from "express";
 import cors from "cors";
-import path from "path"; 
+import path from "path";
 import cookieParser from "cookie-parser"; // 🚀 NAYA: Cookie padhne ke liye tool
 import connectDB from "./config/db.js";
 import seedAdmin from "./utils/seedAdmin.js";
@@ -13,7 +13,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
-import quoteRoutes from "./routes/quoteRoutes.js"; 
+import quoteRoutes from "./routes/quoteRoutes.js";
 import shippingRoutes from "./routes/shippingRoutes.js";
 import layoutRoutes from "./routes/layoutRoutes.js";
 
@@ -25,10 +25,10 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", 
+      "http://localhost:5173",
       "http://localhost:5000",
-      "https://teztech-mern-ecommerce-ixig.vercel.app", 
-      "https://www.sonanielectronics.in", 
+      "https://teztech-mern-ecommerce-ixig.vercel.app",
+      "https://www.sonanielectronics.in",
       "https://sonanielectronics.in",
       "https://poly.teztech.in"
     ],
@@ -56,7 +56,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/quote", quoteRoutes); 
+app.use("/api/quote", quoteRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/shipping", shippingRoutes);
 app.use("/api/layout", layoutRoutes);
